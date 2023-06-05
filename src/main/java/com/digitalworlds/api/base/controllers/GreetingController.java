5734c2a1d961b3c0eae0ab08213e9ea5cfec1044
Lean <lean@MBP-de-Lean.fibertel.com.ar>
2023-06-05 10:05:54 -0300

    Adding CORS

--- src/main/java/com/digitalworlds/api/base/controllers/GreetingController.java
+++ src/main/java/com/digitalworlds/api/base/controllers/GreetingController.java
@@ -1,6 +1,7 @@
 package com.digitalworlds.api.base.controllers;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -11,6 +12,12 @@ public class GreetingController {
     @GetMapping("/greeting")
     public String greeting() {
         return "Hello world!";
+    }
+
+    @PostMapping("/greeting")
+    public void postGreeting() {
+        System.out.println("abc");
+        // ...
     }
 
 }
