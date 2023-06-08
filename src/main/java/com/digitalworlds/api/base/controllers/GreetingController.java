c338e4240cbd4c20911ad30a180b0c53a2bd7e21
Lean <lean@MBP-de-Lean.fibertel.com.ar>
2023-06-08 11:26:52 -0300

    Adding http client and call

--- src/main/java/com/digitalworlds/api/base/controllers/GreetingController.java
+++ src/main/java/com/digitalworlds/api/base/controllers/GreetingController.java
@@ -4,6 +4,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
 
 @RestController
 @RequestMapping("/api")
@@ -12,6 +13,14 @@ public class GreetingController {
     @GetMapping("/greeting")
     public String greeting() {
         return "Hello world!";
+    }
+
+    @GetMapping("/data")
+    public String getExternalData() {
+        RestTemplate client = new RestTemplate();
+        String response = client.getForObject("https://www.cultura.gob.ar/api/v2.0/", String.class);
+
+        return response;
     }
 
     @PostMapping("/greeting")
